34f8077a79e418f65b861ce84f293f6b083d1278
Jukka Nikki <jukka.nikki@kolumbus.fi>
2022-01-13 21:33:17 +0100

    as compact unit test as it goes

--- src/test/kotlin/com/nikkijuk/pigeongram/web/SignatureControllerTest.kt
+++ src/test/kotlin/com/nikkijuk/pigeongram/web/SignatureControllerTest.kt
@@ -7,38 +7,23 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.hateoas.EntityModel
 import org.springframework.http.ResponseEntity
 
 internal class SignatureControllerTest {
 
-    private lateinit var service: SignatureService
-    private lateinit var assembler: SignatureModelAssembler
-
-    private lateinit var controller: SignatureController
-
-    @BeforeEach
-    fun before() {
-        service = mockk()
-        assembler = SignatureModelAssembler ()
-        controller = SignatureController(service,assembler)
-    }
+    private val service: SignatureService = mockk()
+    private val controller = SignatureController (service, SignatureModelAssembler ())
+    private val signature = Signature (id = 1, mailboxId = "m", name = "n", content = "c")
 
     @Test
     fun `creating is successful`() {
-
-        val signature  = Signature (id = 1, mailboxId = "mid", name = "name", content = "content")
-
         every { service.createSignature(any()) } returns signature.toDomain()
 
         val result: ResponseEntity<EntityModel<Signature>> = controller.create(signature)
-
         Assertions.assertFalse(result.body!!.links.isEmpty)
 
         verify { service.createSignature(any()) }
-
     }
-
 }

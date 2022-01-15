82ad7a150fbc299692f902654c3b7c1cd35bc60e
Jukka Nikki <jukka.nikki@kolumbus.fi>
2022-01-15 08:14:56 +0100

    add test and mocking at http level

--- src/test/kotlin/com/nikkijuk/pigeongram/web/SignatureControllerIntegrationTest.kt
+++ src/test/kotlin/com/nikkijuk/pigeongram/web/SignatureControllerIntegrationTest.kt
@@ -4,7 +4,6 @@ import com.nikkijuk.pigeongram.domain.service.SignatureService
 import com.nikkijuk.pigeongram.web.model.toDomain
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
@@ -21,34 +20,23 @@ import com.nikkijuk.pigeongram.domain.model.Signature as SignatureDomain
 import com.nikkijuk.pigeongram.web.model.Signature as SignatureApi
 
 @WebMvcTest(SignatureController::class)
-@ContextConfiguration(classes = [SignatureTestApplication::class, SignatureModelAssembler::class, SignatureService::class])
+@ContextConfiguration(classes =
+    [SignatureTestApplication::class, SignatureModelAssembler::class, SignatureService::class])
 @TestInstance(PER_CLASS)
 internal class SignatureControllerIntegrationTest {
+    @Autowired private lateinit var mockMvc: MockMvc
+    @MockBean private lateinit var signaturService: SignatureService
 
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @MockBean
-    private lateinit var signaturService: SignatureService
-
-    private val requestPayload: SignatureApi = SignatureApi(1, "i", name = "n", content = "c")
+    private val requestPayload = SignatureApi(1, "i", name = "n", content = "c")
     private val mockedServiceResponse: SignatureDomain = requestPayload.toDomain()
 
-    @BeforeAll
-    fun `Create test data`() {
-    }
-
-    @Test
-    fun `Creating a Signature with mocked service SOULD return OK`() {
+    @Test fun `Creating a Signature with mocked service SOULD return CREATED`() {
         whenever(signaturService.createSignature(any())).thenReturn(mockedServiceResponse)
 
         mockMvc.post("/signatures/") {
             contentType = MediaType.APPLICATION_JSON
             accept = MediaType.APPLICATION_JSON
             content = Json.encodeToString(requestPayload)
-        }.andDo { print() }
-        .andExpect { status { isCreated() } }
-
+        }.andExpect { status { isCreated() } }
     }
-
 }
